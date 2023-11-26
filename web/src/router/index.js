5459d24cf4bfbd13c50252a6c3faade845e6affc
Ryugi62 <xorjf1027@naver.com>
2023-11-26 17:11:45 +0900

    feat : redirect  login page when get req to /login

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import IntroView from "../views/IntroView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/intro",
     name: "intro",
     component: IntroView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
 ];
 
---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style scoped></style>
