a2c865ff61066c1622b6e91c6c3571f36f90422a
Ryugi62 <xorjf1027@naver.com>
2023-11-26 21:20:39 +0900

    design : create register view

---
+++ web/src/views/RegisterView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="register">
+    <HeaderComponent />
+
+    <div class="container">
+      <h1>Register</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
+export default {
+  name: "RegisterView",
+
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style scoped></style>
