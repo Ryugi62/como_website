602c5c68e81d2f257de09eee1ab22a6dcf835295
Ryugi62 <xorjf1027@naver.com>
2023-11-26 14:20:32 +0900

    feat : prevention of cors error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 // create como_backend
+const cors = require("cors");
 const express = require("express");
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // CORS 에러 해결
+app.use(express.static(__dirname + "/dist")); // .dist 폴더에 있는 파일들을 사용할 수 있도록 설정
+
+// index.html 파일을 사용할 수 있도록 설정
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
