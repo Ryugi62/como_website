0d1357396da4643e56b1c5177dcd455157d6d8b1
Ryugi62 <xorjf1027@naver.com>
2023-12-04 10:01:32 +0900

    design : create admin path and page

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -6,6 +6,7 @@ import PricingView from "../views/PricingView.vue";
 import DownloadView from "../views/DownloadView.vue";
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
+import AdminView from "../views/AdminView.vue";
 
 const routes = [
   {
@@ -42,6 +43,11 @@ const routes = [
     path: "/register",
     name: "register",
     component: RegisterView,
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: AdminView,
   },
 ];
 
---
+++ web/src/views/AdminView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="download-view">
+    <HeaderComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  name: "RegisterView",
+
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style scoped></style>
