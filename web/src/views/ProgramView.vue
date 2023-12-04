f432c5eb690636c19ad55dca46647ed6367cb9bc
Ryugi62 <xorjf1027@naver.com>
2023-12-04 14:14:14 +0900

    design : create program view design

--- web/src/views/ProgramView.vue
+++ web/src/views/ProgramView.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="program-view">
     <HeaderComponent />
+
+    <div class="content">
+      <h1 class="content-title">COMO PROGRAM의 특징</h1>
+
+      <div class="feature-container">
+        <div class="feature-explanations">
+          <p
+            v-for="explanation in explanations"
+            :key="explanation"
+            class="feature-explanation"
+          >
+            <i class="fas fa-check"></i>
+            {{ explanation }}
+          </p>
+        </div>
+
+        <div class="features">
+          <div class="feature" v-for="feature in features" :key="feature.title">
+            <i :class="feature.icon"></i>
+            <p>{{ feature.title }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   <FooterComponent />
 </template>
@@ -11,16 +35,140 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
   name: "ProgramView",
-
   components: {
     HeaderComponent,
     FooterComponent,
+  },
+  data() {
+    return {
+      explanations: [
+        "대량의 봇을 손쉽게 구동 할 수 있습니다.",
+
+        "모든 시장 상황에 대응이 가능합니다.",
+
+        "DCA투자 방식으로 손실을 최소화 합니다.",
+
+        "매매 과정이 투명합니다.",
+
+        "백데이타를 활용해 내 투자 방식을 시뮬레이션 해볼 수 있습니다.",
+
+        "시뮬레이션 데이타를 차트로 시각화 할 수 있습니다.",
+
+        "예측이 아닌 대응을 전제로 합니다.",
+
+        "시간 이벤트 매매를 대응 할 수 있습니다.",
+
+        "텔레그램으로 실시간 매매 상황을 볼 수 있습니다.",
+
+        "다양한 매매 전략을 적용할 수 있습니다.",
+
+        "초보부터 고급 투자자까지 모두 케어 할 수 있는 정책을 가지고 있습니다.",
+
+        "자금을 매우 효율적으로 관리 할 수 있습니다. (자동 볼기 투자 가능)",
+
+        "상대방의 투자 방식을 손쉽게 적용 할 수 있습니다.",
+
+        "1 ~ 2의 낮은 레버리지로도 충분한 수익이 가능합니다.",
+
+        "확장성이 매우 뛰어나 타 거래소 및 DEX거래소에도 쉽게 적용 가능합니다.",
+
+        "바이낸스 웨건을 지원하여 추가 수익을 도모 할 수 있습니다.",
+
+        "뛰어난 파일럿을 위해 인터벤션 모드와 연습모드를 지원합니다.",
+      ],
+
+      features: [
+        {
+          title: "자동 거래",
+          icon: "fas fa-robot",
+        },
+        {
+          title: "추적 주문",
+          icon: "fas fa-search-dollar",
+        },
+        {
+          title: "DCA",
+          icon: "fas fa-chart-line",
+        },
+        {
+          title: "가상 거래",
+          icon: "fas fa-chart-pie",
+        },
+        {
+          title: "백테스팅",
+          icon: "fas fa-chart-bar",
+        },
+        {
+          title: "전략 디자이너",
+          icon: "fas fa-palette",
+        },
+        {
+          title: "토너먼트",
+          icon: "fas fa-trophy",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style scoped>
 * {
-  border: 1px solid red;
+  /* border: 1px solid red; */
+}
+
+.program-view {
+  margin: auto;
+  max-width: 1440px;
+}
+
+.content {
+  padding: 60px 20px;
+  text-align: center; /* 중앙 정렬 */
+}
+
+.content-title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 80px; /* 여백 */
+}
+
+.feature-container {
+  display: flex;
+  margin-top: 20px;
+  justify-content: space-between; /* 항목 간격 */
+}
+
+.feature-explanations,
+.features {
+  flex: 1;
+  margin: 0 10px; /* 양쪽 여백 */
+}
+
+.feature-explanation,
+.feature {
+  margin: 10px 0;
+  display: flex;
+  padding: 15px;
+  background: #333;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  align-items: center;
+  border-radius: 8px;
+  justify-content: center;
+}
+
+.feature-explanation i,
+.feature i {
+  color: #ffc025; /* 아이콘 색상 */
+  margin-right: 10px;
+}
+
+@media screen and (max-width: 768px) {
+  .feature-container {
+    flex-direction: column-reverse;
+  }
+  .content-title {
+    font-size: 1.5rem;
+  }
 }
 </style>
