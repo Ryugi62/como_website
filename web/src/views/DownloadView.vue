d6e868bdf590f6221e87c4071cf252a33c82f6a6
Ryugi62 <xorjf1027@naver.com>
2023-12-15 15:36:13 +0900

    design : change download page design

--- web/src/views/DownloadView.vue
+++ web/src/views/DownloadView.vue
@@ -2,19 +2,16 @@
   <HeaderComponent />
 
   <main>
-    <section class="hooking-section">
-      <h1 class="hooking-title">
-        <p class="hooking-main-title">"가격은 누구도 예측할 수 없다."</p>
-        <p class="hooking-main-title">"투자에선 예측보다 대응이 중요하다."</p>
+    <section class="section hooking-section">
+      <h1 class="title hooking-title">
+        <p class="main-title">"가격은 누구도 예측할 수 없다."</p>
+        <p class="main-title">"투자에선 예측보다 대응이 중요하다."</p>
       </h1>
-      <p class="hooking-subtitle">- 워렌 버핏 -</p>
-
-      <p class="hooking-subtitle">
-        코모의 트레이딩 봇으로 기술력을 경험해보세요.
-      </p>
+      <p class="subtitle">- 워렌 버핏 -</p>
+      <p class="description">코모의 트레이딩 봇으로 기술력을 경험해보세요.</p>
     </section>
 
-    <section class="bot-select-section">
+    <section class="section bot-select-section">
       <div
         class="bot-select-item"
         :class="{ active: index === activeBotIndex }"
@@ -22,49 +19,28 @@
         :key="index"
         @click="activeBotIndex = index"
       >
-        <!-- <p>{{ bot }}</p> -->
-
         <p>{{ bot }}</p>
       </div>
     </section>
 
-    <section class="duration-select-section">
-      <h1 class="hooking-title">
-        <strong>Trade Titan_binance_{{ botList[activeBotIndex] }}</strong
-        >의 기간을 선택해주세요.
+    <section class="section duration-select-section">
+      <h1 class="title">
+        Trade Titan_binance_{{ botList[activeBotIndex] }}의 기간을 선택해주세요.
       </h1>
-
-      <p class="hooking-subtitle">체험판은 회원가입후 사용하실 수 있습니다</p>
-
-      <div class="duration-select-item">
+      <p class="description">체험판은 회원가입후 사용하실 수 있습니다</p>
+      <div
+        class="duration-select-item"
+        v-for="(duration, index) in durations"
+        :key="index"
+      >
         <div class="duration-text">
           <h2>
             바이낸스 봇 Trade Titan_binance_{{ botList[activeBotIndex] }}
-            <strong>1 YEAR</strong>
+            <strong>{{ duration.period }}</strong>
           </h2>
-
-          <p>
-            ＊해당 체험판의 사용기간은 1년 이며 비트코인에 한정하여 코모의
-            트레이딩봇을 써보실수 있습니다
-          </p>
+          <p>{{ duration.description }}</p>
         </div>
-
-        <div class="button download-button">다운로드</div>
-      </div>
-      <div class="duration-select-item">
-        <div class="duration-text">
-          <h2>
-            바이낸스 봇 Trade Titan_binance_{{ botList[activeBotIndex] }}
-            <strong>1 MONTH</strong>
-          </h2>
-
-          <p>
-            ＊해당 체험판의 사용기간은 1달 이며 모든 코인에 대하여 코모의
-            트레이딩봇을 써보실수 있습니다
-          </p>
-        </div>
-
-        <div class="button download-button">다운로드</div>
+        <button class="button download-button">다운로드</button>
       </div>
     </section>
   </main>
@@ -78,160 +54,178 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
   name: "RegisterView",
-
   components: {
     HeaderComponent,
     FooterComponent,
   },
-
   data() {
     return {
-      botList: [
-        // "바이낸스 봇 (Trade Titan_binance_SPOT)",
-        // "바이낸스 봇 (Trade Titan_binance_FUTURE)",
-        // "바이낸스 봇 (Trade Titan_binance_UPBIT)",
-        // "바이낸스 봇 (Trade Titan_binance_BITHUMB)",
-        // "바이낸스 봇 (Trade Titan_binance_ARBI)",
-        // "바이낸스 봇 (Trade Titan_binance_NOTICE)",
-        "SPOT",
-        "FUTURE",
-        "UPBIT",
-        "BITHUMB",
-        "ARBI",
-        "NOTICE",
+      botList: ["SPOT", "FUTURE", "UPBIT", "BITHUMB", "ARBI", "NOTICE"],
+      activeBotIndex: 0,
+      durations: [
+        {
+          period: "1 YEAR",
+          description: "1년 기간 동안 비트코인 한정으로 사용 가능",
+        },
+        {
+          period: "1 MONTH",
+          description: "1달 기간 동안 모든 코인에 대해 사용 가능",
+        },
       ],
-      activeBotIndex: 1,
     };
   },
 };
 </script>
 
 <style scoped>
-main,
-main * {
-  /* border: 1px solid red; */
-}
-
 main {
-  gap: 50px;
-  margin: 100px auto;
+  margin: 0 auto;
   padding: 20px;
-  display: flex;
   max-width: 1440px;
-  flex-direction: column;
-}
-
-section {
-  padding: 20px 0;
-}
-
-.hooking-section {
   display: flex;
-  align-items: center;
   flex-direction: column;
+  gap: 50px;
 }
 
-.hooking-title {
-  font-size: 40px;
-  font-weight: 700;
+.section {
+  padding: 20px;
   text-align: center;
 }
-.hooking-title strong {
-  color: #ffc025;
+
+.title {
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 10px;
 }
 
-.hooking-main-title {
-  margin: 0;
+.main-title {
+  color: #e0e0e0;
+  margin: 5px 0;
 }
 
-.hooking-subtitle {
-  font-size: 20px;
-  text-align: center;
-  font-weight: 700;
+.subtitle,
+.description {
+  color: #cccccc;
+  font-size: 18px;
+  margin-top: 10px;
 }
 
 .bot-select-section {
   display: flex;
-  justify-content: center;
-  gap: 20px;
-  width: 100%;
-  display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  gap: 20px;
 }
 
 .bot-select-item {
-  width: 150px;
-  height: 150px;
+  width: 200px;
+  height: 200px;
+  color: #ffffff;
   cursor: pointer;
-  border: 1px solid #333333;
   display: flex;
-  transition: all 0.3s ease-in-out;
-  text-align: center;
+  transition: all 0.3s;
   align-items: center;
-  border-radius: 50%;
   justify-content: center;
+  border-radius: 50%;
+  background-color: #202020;
 }
+
 .bot-select-item:hover {
-  box-shadow: 0px 0px 10px 0px #ffffff40;
-  background-color: #212121;
-  font-size: 1.5rem;
+  transform: scale(1.05);
+  background-color: #333333;
 }
 
 .bot-select-item.active {
-  border: 1px solid;
-  background-color: #333333;
-  box-shadow: 0px 0px 10px 0px #ffffffbf;
   transform: scale(1.1);
-  font-size: 1.6rem;
-  font-weight: 700;
+  font-weight: bold;
+  background-color: #ffc025;
+}
+
+.duration-select-section {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
 .duration-select-item {
-  gap: 20px;
   display: flex;
+  flex-direction: row;
+  border: 1px solid #444444;
   padding: 20px;
-  border-radius: 15px;
+  border-radius: 10px;
+  background-color: #202020;
 }
 
 .duration-text {
-  gap: 20px;
-  flex: 0.8;
-  border: 1px solid #333333;
-  padding: 20px;
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
 }
-.duration-text strong {
-  color: #ffc025;
-}
-
-.duration-text h2 {
-  margin: 0;
-  font-size: 30px;
-  font-weight: 700;
-}
-
-.duration-text p {
-  margin: 0;
-  font-size: 16px;
-  font-weight: 400;
-}
 
 .download-button {
-  flex: 0.2;
+  color: white;
   height: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  border: none;
+  cursor: pointer;
+  padding: 10px 20px;
+  border-radius: 5px;
   background-color: #333333;
 }
+
 .download-button:hover {
-  border: 1px solid #7b7b7b;
-  background-color: #212121;
+  background-color: #444444;
 }
+
 .download-button:active {
-  border: 1px solid #ffffff;
-  background-color: #1a1a1a;
+  background-color: #555555;
+}
+
+/* Media Queries for Responsiveness */
+@media (max-width: 768px) {
+  .title {
+    font-size: 24px;
+  }
+
+  .main-title,
+  .subtitle,
+  .description {
+    font-size: 16px;
+  }
+
+  .bot-select-item {
+    width: 150px;
+    height: 150px;
+  }
+
+  .duration-select-item {
+    flex-direction: column;
+  }
+
+  .download-button {
+    width: 100%;
+    margin-top: 10px;
+  }
+}
+
+@media (max-width: 480px) {
+  .title {
+    font-size: 20px;
+  }
+
+  .main-title,
+  .subtitle,
+  .description {
+    font-size: 14px;
+  }
+
+  .bot-select-section {
+    justify-content: space-around;
+  }
+
+  .bot-select-item {
+    width: 120px;
+    height: 120px;
+  }
 }
 </style>
