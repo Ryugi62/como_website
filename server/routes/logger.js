0e37ccbb9761704c3be50e50dd795620b94315d3
Ryugi62 <xorjf1027@naver.com>
2024-01-02 09:01:48 +0900

    fix : add logs file in git ignore file

--- server/routes/logger.js
+++ server/routes/logger.js
@@ -5,7 +5,7 @@ function log(type, message, details) {
   const timestamp = new Date().toISOString();
   const detailString = JSON.stringify(details, null, 4); // 세부 정보를 보기 좋게 포맷팅
   const logMessage = `${timestamp}\n${type.toUpperCase()}\n${message}\nDetails: ${detailString}\n\n`;
-  fs.appendFileSync(path.join(__dirname, "logs.txt"), logMessage);
+  fs.appendFileSync(path.join(__dirname, "logs/logs.txt"), logMessage);
 }
 
 module.exports = { log };
