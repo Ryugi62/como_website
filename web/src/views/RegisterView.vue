71488d9a40e8f4446dbb954cde5cc197fe49580c
Ryugi62 <xorjf1027@naver.com>
2024-01-02 13:06:51 +0900

    refactor : change to clean code.

--- web/src/views/RegisterView.vue
+++ web/src/views/RegisterView.vue
@@ -1,34 +1,36 @@
 <template>
   <section class="registration-section">
+    <!-- 이미지 컨테이너 -->
     <div class="image-container">
       <img src="@/assets/img/register-bitcoin.png" alt="Registration Image" />
     </div>
 
+    <!-- 등록 양식 -->
     <article class="registration-article">
       <form @submit.prevent="handleRegistration" class="registration-form">
         <h1 class="form-title">Register</h1>
         <p class="form-description">Create your new account</p>
 
+        <!-- 아이디 입력 필드 -->
         <div class="input-wrapper">
           <label for="userId">Choose a userId</label>
-          <div class="userid-input">
-            <input
-              id="userId"
-              v-model="userId"
-              type="text"
-              class="input-field"
-              placeholder="User ID"
-              @blur="checkUserIdAvailability"
-            />
-          </div>
+          <input
+            id="userId"
+            v-model="userId"
+            type="text"
+            class="input-field"
+            placeholder="User ID"
+            @blur="validateUserId"
+          />
           <span
             v-if="userIdMessage"
             class="message"
-            :class="{ error: !isUserIdAvailable }"
+            :class="{ error: !isUserIdValid }"
             >{{ userIdMessage }}</span
           >
         </div>
 
+        <!-- 이메일 입력 필드 -->
         <div class="input-wrapper">
           <label for="email">Enter your email address</label>
           <input
@@ -37,9 +39,17 @@
             type="email"
             class="input-field"
             placeholder="Email"
+            @blur="validateEmail"
           />
+          <span
+            v-if="emailMessage"
+            class="message"
+            :class="{ error: !isEmailValid }"
+            >{{ emailMessage }}</span
+          >
         </div>
 
+        <!-- 비밀번호 입력 필드 -->
         <div class="input-wrapper">
           <label for="password">Choose a password</label>
           <input
@@ -48,9 +58,17 @@
             type="password"
             class="input-field"
             placeholder="Password"
+            @blur="validatePassword"
           />
+          <span
+            v-if="passwordMessage"
+            class="message"
+            :class="{ error: !isPasswordValid }"
+            >{{ passwordMessage }}</span
+          >
         </div>
 
+        <!-- 비밀번호 확인 입력 필드 -->
         <div class="input-wrapper">
           <label for="confirm-password">Confirm your password</label>
           <input
@@ -59,9 +77,17 @@
             type="password"
             class="input-field"
             placeholder="Confirm Password"
+            @blur="validateConfirmPassword"
           />
+          <span
+            v-if="confirmPasswordMessage"
+            class="message"
+            :class="{ error: !isConfirmPasswordValid }"
+            >{{ confirmPasswordMessage }}</span
+          >
         </div>
 
+        <!-- 전화번호 입력 필드 -->
         <div class="input-wrapper">
           <label for="phone">Enter your phone number</label>
           <div class="phone-input">
@@ -71,6 +97,7 @@
               type="tel"
               class="input-field"
               placeholder="XXX"
+              @blur="validatePhone"
             />
             <span>-</span>
             <input
@@ -79,6 +106,7 @@
               type="tel"
               class="input-field"
               placeholder="XXXX"
+              @blur="validatePhone"
             />
             <span>-</span>
             <input
@@ -87,10 +115,18 @@
               type="tel"
               class="input-field"
               placeholder="XXXX"
+              @blur="validatePhone"
             />
           </div>
+          <span
+            v-if="phoneMessage"
+            class="message"
+            :class="{ error: !isPhoneValid }"
+            >{{ phoneMessage }}</span
+          >
         </div>
 
+        <!-- 등록 버튼 -->
         <button type="submit" class="button submit-button">Register</button>
         <div class="registration-prompt">
           Already have an account?
@@ -104,6 +140,7 @@
 <script>
 export default {
   name: "RegisterView",
+
   data() {
     return {
       userId: "",
@@ -113,10 +150,19 @@ export default {
       phone1: "",
       phone2: "",
       phone3: "",
-      isUserIdAvailable: null,
+      isUserIdValid: true,
       userIdMessage: "",
+      isEmailValid: true,
+      emailMessage: "",
+      isPasswordValid: true,
+      passwordMessage: "",
+      isConfirmPasswordValid: true,
+      confirmPasswordMessage: "",
+      isPhoneValid: true,
+      phoneMessage: "",
     };
   },
+
   methods: {
     async handleRegistration() {
       if (!this.validateForm()) return;
@@ -129,99 +175,146 @@ export default {
           phone: `${this.phone1}-${this.phone2}-${this.phone3}`,
         };
 
-        const isRegistered = await this.$store.dispatch(
+        // 여기에 서버로 데이터를 전송하는 코드를 작성하십시오.
+        const isRegistrationSuccessful = await this.$store.dispatch(
           "register",
           registrationData
         );
 
-        if (isRegistered) {
-          alert("회원가입에 성공했습니다.");
+        if (isRegistrationSuccessful) {
           this.$router.push("/login");
         } else {
-          alert("회원가입에 실패했습니다.");
+          alert("등록에 실패했습니다.");
         }
       } catch (error) {
-        alert("회원가입에 실패했습니다.");
+        console.error("Registration failed:", error);
       }
     },
 
     validateForm() {
+      this.validateUserId();
+      this.validateEmail();
+      this.validatePassword();
+      this.validateConfirmPassword();
+      this.validatePhone();
+
+      return (
+        this.isUserIdValid &&
+        this.isEmailValid &&
+        this.isPasswordValid &&
+        this.isConfirmPasswordValid &&
+        this.isPhoneValid
+      );
+    },
+
+    async validateUserId() {
+      this.isUserIdValid = true;
+      this.userIdMessage = "";
+
       if (!this.userId) {
-        alert("아이디를 입력해주세요.");
-        return false;
-      } else if (this.isUserIdAvailable !== true) {
-        alert("아이디 중복확인을 해주세요.");
-        return false;
-      } else if (this.userId.length < 4 || this.userId.length > 20) {
-        alert("아이디는 4자 이상 20자 이하로 입력해주세요.");
-        return false;
-      } else if (!/^[a-z0-9-_]{4,20}$/.test(this.userId)) {
-        alert("아이디는 영문 소문자, 숫자, 특수기호(_)(-)만 사용 가능합니다.");
-        return false;
+        this.isUserIdValid = false;
+        this.userIdMessage = "아이디를 입력해주세요.";
+        return;
       }
+
+      // 아이디 길이 및 문자 유효성 검사
+      if (this.userId.length < 4 || this.userId.length > 20) {
+        this.isUserIdValid = false;
+        this.userIdMessage = "아이디는 4자 이상 20자 이하로 입력해주세요.";
+        return;
+      }
+
+      if (!/^[a-zA-Z0-9-_]+$/.test(this.userId)) {
+        this.isUserIdValid = false;
+        this.userIdMessage =
+          "아이디는 영문자, 숫자, 밑줄(_), 대시(-)만 사용할 수 있습니다.";
+      }
+
+      const isUserIdAvailable = await this.$store.dispatch(
+        "checkUserIdAvailability",
+        this.userId
+      );
+      if (!isUserIdAvailable) {
+        this.isUserIdValid = false;
+        this.userIdMessage = "이미 사용 중인 아이디입니다.";
+      }
+    },
+
+    validateEmail() {
+      this.isEmailValid = true;
+      this.emailMessage = "";
 
       if (!this.email) {
-        alert("이메일을 입력해주세요.");
-        return false;
-      } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(this.email)) {
-        alert("이메일 형식이 올바르지 않습니다.");
-        return false;
+        this.isEmailValid = false;
+        this.emailMessage = "이메일을 입력해주세요.";
+        return;
       }
 
+      // 이메일 형식 검사
+      if (
+        !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(this.email)
+      ) {
+        this.isEmailValid = false;
+        this.emailMessage = "유효한 이메일 주소를 입력해주세요.";
+      }
+    },
+
+    validatePassword() {
+      this.isPasswordValid = true;
+      this.passwordMessage = "";
+
       if (!this.password) {
-        alert("비밀번호를 입력해주세요.");
-        return false;
-      } else if (this.password.length < 8 || this.password.length > 20) {
-        alert("비밀번호는 8자 이상 20자 이하로 입력해주세요.");
-        return false;
-      } else if (
-        !/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(
+        this.isPasswordValid = false;
+        this.passwordMessage = "비밀번호를 입력해주세요.";
+        return;
+      }
+
+      // 비밀번호 길이 및 복잡성 검사
+      if (this.password.length < 8 || this.password.length > 20) {
+        this.isPasswordValid = false;
+        this.passwordMessage = "비밀번호는 8자 이상 20자 이하로 입력해주세요.";
+        return;
+      }
+
+      if (
+        !/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,20}$/.test(
           this.password
         )
       ) {
-        alert(
-          "비밀번호는 영문자, 숫자, 특수문자를 모두 포함해야 합니다. (공백은 사용할 수 없습니다.)"
-        );
-        return false;
-      } else if (this.password !== this.confirmPassword) {
-        alert("비밀번호가 일치하지 않습니다.");
-        return false;
+        this.isPasswordValid = false;
+        this.passwordMessage =
+          "비밀번호는 영문자, 숫자, 특수문자를 포함해야 합니다.";
       }
+    },
+
+    validateConfirmPassword() {
+      this.isConfirmPasswordValid = true;
+      this.confirmPasswordMessage = "";
+
+      if (this.password !== this.confirmPassword) {
+        this.isConfirmPasswordValid = false;
+        this.confirmPasswordMessage = "비밀번호가 일치하지 않습니다.";
+      }
+    },
+
+    validatePhone() {
+      this.isPhoneValid = true;
+      this.phoneMessage = "";
 
       if (!this.phone1 || !this.phone2 || !this.phone3) {
-        alert("전화번호를 입력해주세요.");
-        return false;
-      } else if (
-        !/^[0-9]{3}$/.test(this.phone1) ||
+        this.isPhoneValid = false;
+        this.phoneMessage = "전화번호를 입력해주세요.";
+        return;
+      }
+
+      // 전화번호 형식 검사
+      if (
+        !/^[0-9]{3,4}$/.test(this.phone1) ||
         !/^[0-9]{4}$/.test(this.phone2) ||
         !/^[0-9]{4}$/.test(this.phone3)
       ) {
-        alert("전화번호 형식이 올바르지 않습니다.");
-        return false;
-      }
-
-      return true;
-    },
-
-    async checkUserIdAvailability() {
-      if (!this.userId) {
-        this.isUserIdAvailable = null;
-        this.userIdMessage = "";
-        return;
-      }
-      try {
-        const isAvailable = await this.$store.dispatch(
-          "checkUserIdAvailability",
-          this.userId
-        );
-
-        this.isUserIdAvailable = isAvailable;
-        this.userIdMessage = isAvailable
-          ? "사용 가능한 아이디입니다."
-          : "이미 존재하는 아이디입니다.";
-      } catch (error) {
-        this.isUserIdAvailable = false;
-        this.userIdMessage = "아이디 확인 중 오류가 발생했습니다.";
+        this.isPhoneValid = false;
+        this.phoneMessage = "유효한 전화번호를 입력해주세요.";
       }
     },
   },
