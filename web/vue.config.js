a5b946bfa36dbc02932dd82e2da6ca83ff84692f
Ryugi62 <xorjf1027@naver.com>
2024-01-05 08:55:04 +0900

    feat : create fit that can send req to live server or localhost

--- web/vue.config.js
+++ web/vue.config.js
@@ -5,7 +5,15 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost",
+        target: () => {
+          if (window.location.hostname === "como.duckdns.org") {
+            return process.env.VUE_APP_SERVER_DNS_URL;
+          } else if (window.location.hostname === "35.237.222.239") {
+            return process.env.VUE_APP_SERVER_URL;
+          } else {
+            return process.env.VUE_APP_SERVER_LOCAL_URL;
+          }
+        },
         changeOrigin: true,
       },
     },
