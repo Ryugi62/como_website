fa0f261d6e4245babfd9610388939cb65fb2a0a0
Ryugi62 <xorjf1027@naver.com>
2024-01-04 14:34:42 +0900

    feat : create feat that can get user information in state.js

--- web/src/store/getters.js
+++ web/src/store/getters.js
@@ -2,4 +2,17 @@ export const getters = {
   isLoggedIn: (state) => state.loggedIn,
 
   isAdmin: (state) => state.user?.role === "admin",
+
+  userEmail: (state) => state.user?.email,
+  isUserAdmin: (state) => state.user?.isAdmin,
+  isUserSubscribed: (state) => state.user?.isSubscribed,
+  userPhone: (state) => state.user?.phone,
+  userReferralLink: (state) => state.user?.referralLink,
+  userSubscriptionAmount: (state) => state.user?.subscriptionAmount,
+  userSubscriptionEndDate: (state) => state.user?.subscriptionEndDate,
+  userSubscriptionProduct: (state) => state.user?.subscriptionProduct,
+  userSubscriptionStartDate: (state) => state.user?.subscriptionStartDate,
+  userTotalPaidAmount: (state) => state.user?.totalPaidAmount,
+  userId: (state) => state.user?.userId,
+  userSupscription: (state) => state.user?.subscription,
 };
