a38096c6bc17ebf25d71c322c0d99882152d0e9b
Ryugi62 <xorjf1027@naver.com>
2024-01-09 09:40:30 +0900

    refactor : change log

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use((err, req, res, next) => {
 
 // log 출력
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url} ${req.path} ${req.ip}`);
+  console.log(`${req.method} ${req.url}`);
   next();
 });
 
